614b4e95da579bb6c24cea674eec0dcb4cd94f31
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-10-20 02:22:17 +0900

    Update Game-of-Thrones---I.cpp

--- Game-of-Thrones---I.cpp
+++ Game-of-Thrones---I.cpp
@@ -9,7 +9,6 @@ using namespace std;
  * The function accepts STRING s as parameter.
  */
 
-
 string gameOfThrones(string s)
 {
     array<int, 26> freq{};
@@ -25,7 +24,8 @@ string gameOfThrones(string s)
     return (oddCount <= 1) ? "YES" : "NO";
 }
 
-string gameOfThrones(string s)
+
+string gameOfThrones2(string s)
 {
     unordered_map<char, int> freq;
 
@@ -40,39 +40,6 @@ string gameOfThrones(string s)
     return (oddCount <= 1) ? "YES" : "NO";
 }
 
-string gameOfThrones(string s)
-{
-    std::string result {"YES"};
-
-    std::map<char, int> mp;
-    
-    for (char c : s)
-    {
-        auto ret = mp.insert({ c, 1 });
-        if (!ret.second)
-        {
-            ret.first->second++;
-        }        
-    }
-
-    int count = 0;
-    for (const auto &item : mp)
-    {
-        int x = item.second;
-        if ( x % 2 != 0)
-        {
-            count++;
-            
-            if (count > 1)
-            {
-                result = "NO";
-                break;
-            }
-        }
-    }
-
-    return result; 
-}
 
 int main()
 {
