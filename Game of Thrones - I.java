6c6214b1a6aab670ab54665c885928e6d5b815aa
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-10-26 01:14:35 +0900

    Update Game of Thrones - I.java

--- Game of Thrones - I.java
+++ Game of Thrones - I.java
@@ -1,1 +1,20 @@
-Game of Thrones - I
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'gameOfThrones' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
