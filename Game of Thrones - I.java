5c4c9c0df45348457d0471d4a7ef2675203862e2
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-10-27 02:35:56 +0900

    Update Game of Thrones - I.java

--- Game of Thrones - I.java
+++ Game of Thrones - I.java
@@ -18,5 +18,20 @@ class Result {
      * The function is expected to return a STRING.
      * The function accepts STRING s as parameter.
      */
+    public static String gameOfThrones(String s) {
+        int[] freq = new int[26];
 
+        for (char c : s.toCharArray()) {
+            freq[c - 'a']++;
+        }
+
+        int oddCount = 0;
+        for (int count : freq) {
+            if (count % 2 != 0)
+                oddCount++;
+        }
+
+        return (oddCount <= 1) ? "YES" : "NO";
+    }
+    
 }
