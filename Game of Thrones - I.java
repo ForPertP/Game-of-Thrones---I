d27f005ac786d962b3e06ae64f678b77a3db0be7
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-10-28 00:45:00 +0900

    Update Game of Thrones - I.java

--- Game of Thrones - I.java
+++ Game of Thrones - I.java
@@ -33,5 +33,22 @@ class Result {
 
         return (oddCount <= 1) ? "YES" : "NO";
     }
-    
+
+
+
+    public static String gameOfThrones2(String s) {
+        Map<Character, Integer> freq = new HashMap<>();
+
+        for (char c : s.toCharArray()) {
+            freq.put(c, freq.getOrDefault(c, 0) + 1);
+        }
+
+        int oddCount = 0;
+        for (int count : freq.values()) {
+            if (count % 2 != 0)
+                oddCount++;
+        }
+
+        return (oddCount <= 1) ? "YES" : "NO";
+    }        
 }
